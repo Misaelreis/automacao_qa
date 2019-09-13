7975e0748c8636396ce76b9d33a8d5f2f600b8f0
Misael Aparecido Reis Cândido <misaelcandido@ENG-317.local>
2019-09-13 09:51:06 -0300

    update

---
+++ spec/daynamic_control_spec.rb
@@ -0,0 +1,14 @@
+describe 'Dynamic control', :dc do
+    before(:each) do
+        visit('/dynamic_controls')
+    end
+
+    it 'Quando habilita o campo' do
+        # Quando tem a propriedade desable true o campo esta desabilitado.
+        res = page.has_field? 'movie', disabled: true
+        puts res
+        click_button 'Habilita'
+        res = page.has_field? 'movie', disabled: false
+        puts res
+    end
+end
